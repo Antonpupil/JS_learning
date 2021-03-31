adc9f6f4ba903d6a47f6490db8092ea4100f1ddf
Antonpupil <70876693+Antonpupil@users.noreply.github.com>
2021-03-31 17:28:44 +0300

    правка скрипта

--- dom/modal/modal.js
+++ dom/modal/modal.js
@@ -2,45 +2,41 @@ const openBtn = document.querySelector("#openOverlay");
 const body = document.body;
 const successModal = createModal("The message has been sent");
 
-openBtn.addEventListener = ("click", e => {
+openBtn.addEventListener("click", (e) => {
     body.appendChild(successModal);
-})
+});
 
-function createModal(content){
+function createModal(content) {
+
     const overlayElement = document.createElement("div");
-    overlayElement.className.add("overlay");
+    overlayElement.classList.add("overlay");
 
-    overlayElement.addEventListener("click", e => {
-        /* console.log(e.target); можно увидеть где произошел клик target указывает на блок с событием*/
-        if (e.target == overlayElement) {
+    overlayElement.addEventListener("click", (e) => {
+        if (!e.target.classList.contains("content")) {
             closeElement.click();
         }
-        /* if(!e.target.classList.contains("content")){
-            closeElement.click();
-        } если  у элемента с событием клик НЕТ класса content, то кликнуть на кнопку закрытия. Если content нет, то в if - true и тогда закрытие*/
-    })
+    });
 
     const containerElement = document.createElement("div");
-    containerElement.className.add("modal-container");
+    containerElement.classList.add("modal-container");
 
     const contentElement = document.createElement("div");
-    contentElement.className.add("content");
+    contentElement.classList.add("content");
 
-    contentElement.innerHTML = content;/* "Hello <b>world<b>!" можно было вставить до использования внутри функции*/
+    contentElement.innerHTML = content;
     const closeElement = document.createElement("a");
-    closeElement.className.add("close");
+    closeElement.classList.add("close");
     closeElement.textContent = 'x';
-
     closeElement.href = "#";
-    closeElement.addEventListener("click", e => {
+
+    closeElement.addEventListener("click", (e) => {
         e.preventDefault();
         body.removeChild(overlayElement);
-    })
-
+    });
 
     overlayElement.appendChild(containerElement);
     containerElement.appendChild(closeElement);
     containerElement.appendChild(contentElement);
 
-    return overlayElement;/* body.appendChild(overlayElement); до применения в функции*/
+    return overlayElement;
 }
